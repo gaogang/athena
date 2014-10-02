4a01077f66a668a461c25d356253c158204ba88a
Gang Gao <gaogang@gmail.com>
2014-10-02 22:54:02 +0100

    Add Android test harness

---
+++ Athena.TestHarness.Android.App/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Athena.TestHarness.Pcl.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+namespace Athena.TestHarness.Android.App
+{
+	[Activity (Label = "Athena.TestHarness.Android.App", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : AndroidActivity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			Forms.Init (this, bundle);
+
+			SetPage (new NavigationPage(new TestHarnessPage ()));
+		}
+	}
+}
+
+
