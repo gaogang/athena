77249860582da1b221d10e798e45edaa511e9f89
Gang Gao <gaogang@gmail.com>
2014-10-02 20:49:39 +0100

    Added GestureArgs.cs

---
+++ Athena.Core.Pcl/Gesture/GestureArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Athena.Core.Pcl.Gesture
+{
+	public class GestureArgs
+	{
+		private readonly GestureState _state;
+
+		public GestureArgs (GestureState state)
+		{
+			_state = state;
+		}
+
+		public GestureState State 
+		{
+			get {
+				return _state;
+			}
+		}
+	}
+}
+
